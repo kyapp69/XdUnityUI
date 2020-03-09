d19457b925dedbbcd312de7ffd9c1540a7017d5f
itouh2 <itouh2@i0plus.jp>
2020-03-09 17:55:55 +0900

    [update] icon image files.

--- XdPlugin/XdUnityUIExport/images/resize.sh
+++ XdPlugin/XdUnityUIExport/images/resize.sh
@@ -1,3 +1,6 @@
-/mnt/c/iola-user/local/ffmpeg-20200225-36451f9-win64-static/bin/ffmpeg.exe -i ./icon@4x.png -s 144:144 ./icon@3x.png
-/mnt/c/iola-user/local/ffmpeg-20200225-36451f9-win64-static/bin/ffmpeg.exe -i ./icon@4x.png -s 48:48 ./icon@2x.png
-/mnt/c/iola-user/local/ffmpeg-20200225-36451f9-win64-static/bin/ffmpeg.exe -i ./icon@4x.png -s 24:24 ./icon@1x.png
+#!/bin/sh
+FFMPEG=/mnt/c/iola-user/local/ffmpeg-20200225-36451f9-win64-static/bin/ffmpeg.exe
+
+${FFMPEG} -i ./icon@4x.png -s 144:144 -y ./icon@3x.png
+${FFMPEG} -i ./icon@4x.png -s 48:48 -y ./icon@2x.png
+${FFMPEG} -i ./icon@4x.png -s 24:24 -y ./icon@1x.png
