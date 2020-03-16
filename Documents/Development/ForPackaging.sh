54719da0345b180f9f51d9a2d0176572028a3b45
itouh2 <itouh2@i0plus.jp>
2020-03-16 11:42:57 +0900

    [update] packaging shell script.

--- Documents/Development/ForPackaging.sh
+++ Documents/Development/ForPackaging.sh
@@ -3,8 +3,8 @@
 
 # Samples.xdの同期をする
 echo "----- sync samples.xd -----"
-SAMPLE1=UnityProject/Assets/I0plus/XdUnityUI-ForAdobeXD/samples.xd
-SAMPLE2=./XdPlugin/SampleXd/samples.xd
+SAMPLE1=UnityProject/Assets/I0plus/XdUnityUI-ForAdobeXD/Samples
+SAMPLE2=./XdPlugin/Samples
 rsync --update --existing ${SAMPLE1} ${SAMPLE2}
 rsync --update --existing ${SAMPLE2} ${SAMPLE1}
 echo "done.\n"
